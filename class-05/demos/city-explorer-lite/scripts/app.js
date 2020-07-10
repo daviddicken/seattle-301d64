253eb39797fadcb4eac7a85658e4527f6982099c
LENA EIVY <applena@gmail.com>
2020-07-10 12:20:39 -0700

    day 5 updated

--- class-05/demos/city-explorer-lite/scripts/app.js
+++ class-05/demos/city-explorer-lite/scripts/app.js
@@ -7,9 +7,11 @@
 // make event handler
 function fetchCityData(event){
   event.preventDefault();
+  $('#map').empty();
+  $('#title').empty();
+  // $('#restaurants').html().empty();
 
   let searchQuery = $('#input-search').val().toLowerCase();
-  console.log('this is our search:', searchQuery);
 
   const ajaxSettings = {
     method:'GET', 
@@ -19,7 +21,40 @@ function fetchCityData(event){
 
   $.ajax('/fake-data/location.json', ajaxSettings)
     .then(dataThatIgetBackFromMyLocationJson => {
-      console.log('data returned from ajax: ', dataThatIgetBackFromMyLocationJson);
+      showTitle(dataThatIgetBackFromMyLocationJson);
+      showMap(dataThatIgetBackFromMyLocationJson);
+      showRestaurants(dataThatIgetBackFromMyLocationJson);
+    })
+}
+
+function showTitle(locationObject){
+  let template = $('#title-template').html();
+  let html = Mustache.render(template, locationObject);
+  $('#title').append(html);
+}
+
+function showMap(locationObject){
+  let template = $('#image-template').html();
+  let html = Mustache.render(template, locationObject);
+  $('#map').append(html);
+}
+
+function showRestaurants(locationObject){
+  const ajaxSettings = {
+    method: "GET",
+    dataType: "JSON",
+    data: locationObject
+  }
+
+  $.ajax('/fake-data/restaurants.json', ajaxSettings)
+    .then(resultsFromRestaurants => {
+      let template = $('#restaurant-results-template').html();
+
+      resultsFromRestaurants.forEach(restaurant => {
+        let html = Mustache.render(template, restaurant);
+        $('#restaurant-results').append(html);
+      })
+
     })
 }
 
